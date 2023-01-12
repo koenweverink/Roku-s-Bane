29b482c278b2ffda9515febd50b4cac0e2ea7e6d
EvaPeet-sp <122272833+EvaPeet-sp@users.noreply.github.com>
2023-01-12 14:50:44 +0100

    Inital skeleton verlet

--- comet.py
+++ comet.py
@@ -1,88 +1,90 @@
 import numpy as np
 
-# Variables 
+class Comet:
 
-# Radius of the Earth
-R = 6371000
+    # Variables 
 
-# Drag coefficient
-C_d = 0.47
+    # Radius of the Earth
+    R = 6371000
 
-# Heat transfer coefficient
-C_h = 0.1
+    # Drag coefficient
+    C_d = 0.47
 
-# Stefan-Boltzmann constant 
-sigma = 5.6697 * 10**-8
+    # Heat transfer coefficient
+    C_h = 0.1
 
-# Temperature in K
-T = 1000
+    # Stefan-Boltzmann constant 
+    sigma = 5.6697 * 10**-8
 
-# Heat of ablation in MJ
-Q = 8
+    # Temperature in K
+    T = 1000
 
-
-# Simple Formulas
-def gravity(h):
-    g = 9.81
-
-    if h != 0:
-        g = g * (1 - (2*h / R))
-
-    return g
+    # Heat of ablation in MJ
+    Q = 8
 
 
-def weight(m, g):
-    return m * g
+    # Simple Formulas
+    def gravity(h):
+        g = 9.81
+
+        if h != 0:
+            g = g * (1 - (2*h / R))
+
+        return g
 
 
-def projected_area(radius):
-    return np.pi * radius**2
+    def weight(m, g):
+        return m * g
 
 
-def air_friction(velocity, radius):
-    air_friction = -0.65 * projected_area(radius) * abs(velocity**2)
-    return air_friction
+    def projected_area(radius):
+        return np.pi * radius**2
 
 
-def total_force(m, g, velocity, radius):
-    total_force = weight(m, g) * air_friction(velocity, radius)
-    return total_force
+    def air_friction(velocity, radius):
+        air_friction = -0.65 * projected_area(radius) * abs(velocity**2)
+        return air_friction
 
 
-def acceleration(m, g, velocity, radius):
-    acceleration = total_force(m, g, velocity, radius) / m
-    return acceleration
+    def total_force(m, g, velocity, radius):
+        total_force = weight(m, g) * air_friction(velocity, radius)
+        return total_force
 
 
-def air_density(h):
-    if h > 25098.76:
-        T = -205.05 + 0.00164 * h
-        P = 51.97 * ((T + 459.7) / 389.98)**-11.388
-
-    if 11019.13 < h < 25098.76:
-        T = -70
-        P = 473.1 * np.exp(1.73 - 0.000048*h)
-
-    if h < 11019.13:
-        T = 59 - 0.00356 * h
-        P = 2116 * (((T + 459.7) / 518.6))**5.256
-
-    air_density = P / (1718 * (T + 459.7))
-    return air_density 
+    def acceleration(m, g, velocity, radius):
+        acceleration = total_force(m, g, velocity, radius) / m
+        return acceleration
 
 
+    def air_density(h):
+        if h > 25098.76:
+            T = -205.05 + 0.00164 * h
+            P = 51.97 * ((T + 459.7) / 389.98)**-11.388
 
-# Complex Formulas
-def change_in_velocity(h, radius, init_V, m, angle):
-    dV_dt = -C_d * ((air_density(h) * projected_area(radius) * init_V**2) / m) + gravity(h) * np.sin(angle)
-    return dV_dt
+        if 11019.13 < h < 25098.76:
+            T = -70
+            P = 473.1 * np.exp(1.73 - 0.000048*h)
+
+        if h < 11019.13:
+            T = 59 - 0.00356 * h
+            P = 2116 * (((T + 459.7) / 518.6))**5.256
+
+        air_density = P / (1718 * (T + 459.7))
+        return air_density 
 
 
-def change_in_mass(radius, h, init_V, m):
-    param1 = (C_h * air_density(h) * init_V**3) / 2 
-    param2 = sigma * T**4
 
-    value = min(param1, param2)
+    # Complex Formulas
+    def change_in_velocity(h: float, radius: float, init_V: int, m: float, angle: int):
+        dV_dt = -C_d * ((air_density(h) * projected_area(radius) * init_V**2) / m) + gravity(h) * np.sin(angle)
+        return dV_dt
 
-    dM_dt = -projected_area(radius) *  (value / (m * Q))
-    return dM_dt
+
+    def change_in_mass(radius, h, init_V, m):
+        param1 = (C_h * air_density(h) * init_V**3) / 2 
+        param2 = sigma * T**4
+
+        value = min(param1, param2)
+
+        dM_dt = -projected_area(radius) *  (value / (m * Q))
+        return dM_dt
--- velocity_verlet.py
+++ velocity_verlet.py
@@ -1,2 +1,63 @@
-#hoiiii
-#besties
+from comet import Comet
+
+"""constants"""
+dt = 0.1 # timestep (accuracy)
+
+
+"""initial conditions"""
+x = [0] # initial position
+v = [1.8E4] # initial velocity
+mass = [10] # intial mass
+total_height = 2E5
+
+# time
+t = [0]
+
+
+"""the approximation"""
+
+
+while x[-1] < total_height: # we should add something for the mass
+    
+    h = total_height - x[-1]
+    acc = Comet.change_in_velocity(h, 2, v[-1], mass[0], 45) # is het v[-1] of v[0] ?????
+
+    """calculate half timestep velocity"""
+    v_half_t = v[-1] + acc * 1/2 * dt
+    
+    """calculate movement over timestep"""
+    x.append(x[-1] + v_half_t * dt)
+
+    """derive new acc for timestep from position"""
+    h2 = total_height - x[-1]
+    acc2 = Comet.change_in_velocity(h2, 2, v[-1], mass[0], 45)
+
+    """calculate velocity for time"""
+    v.append(v_half_t + 1/2 * acc2 * dt)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def euler_approx(maxim: int, step_size: float, t: int, x: int, i:int, start_val: int) -> list[float]:
+#     arr_steps : list[float] = []
+#     arr_steps.append(start_val)
+
+#     step_num = int(1/step_size)
+
+#     for step in [i * step_size for i in range(1, step_num * maxim + 1)]:
+#         new_val = arr_steps[-1] + (t * step + x * arr_steps[-1] + i) * step_size
+#         arr_steps.append(new_val)
+        
+#     return arr_steps
+
